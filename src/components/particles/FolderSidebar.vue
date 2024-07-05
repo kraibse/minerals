64008aa67936a37ad0e515089572af2bd3181f5f
kraibse <33869366+kraibse@users.noreply.github.com>
2024-07-05 14:40:17 +0200

    Updated initial template to tauri v1

---
+++ src/components/particles/FolderSidebar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    <div id="folder-sidebar">
+        <h1>Sidebar </h1>
+    </div>
+</template>
+
+<style scoped>
+#folder-sidebar {
+    max-width: 400px;
+    width: 240px;
+    height: 100vh;
+
+    background-color: #23272e;
+}
+</style>
