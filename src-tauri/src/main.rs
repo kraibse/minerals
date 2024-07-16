258223db6f2d4416508b1c2c7c3125ab33abe1eb
kraibse <33869366+kraibse@users.noreply.github.com>
2024-07-16 16:58:10 +0200

    Added fs command to rust source

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,60 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::fs;
+use std::path::Path;
+
+#[derive(serde::Serialize)]
+struct FileItem {
+    name: String,
+    is_dir: bool,
+    // Add other file attributes as needed
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn read_directory(path: String) -> Result<Vec<FileItem>, String> {
+    let path = Path::new(&path);
+
+    if !path.exists() {
+        return Err("Path does not exist".into());
+    }
+
+    // if !path.is_dir() {
+    //     return Err("Path is not a directory".into());
+    // }
+
+    match fs::read_dir(path) {
+        Ok(entries) => {
+            let items: Vec<FileItem> = entries
+                .filter_map(|entry| {
+                    entry.ok().map(|e| {
+                        let file_type = e.file_type().ok()?;
+                        let name = e.file_name().into_string().ok()?;
+                        Some(FileItem {
+                            name,
+                            is_dir: file_type.is_dir(),
+                            // Add other file attributes here
+                        })
+                    })
+                })
+                .flatten()
+                .collect();
+            Ok(items)
+        }
+        Err(e) => Err(e.to_string()),
+    }
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![read_directory])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'active': '#4285F4',
+        'active-mute': '#4286f4',
+        'primary': '#23272e',
+        'alternate': '#1e2227',
+        'highlighted': '#2d313b',
+      },
+    },
   },
   plugins: [],
 }
